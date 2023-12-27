5dd32e10503ebccfbf22a2dad1128066208bf564
Divyansh Gupta <divyanshguptaekana123@gmail.com>
2023-12-27 16:09:01 +0530

    chore(hello_cargo): learn struct in rust

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,37 +1,64 @@
-use std::io;
-fn main(){
-let my_string= String::from("hello world");
-
-
-let word= first_word(&my_string[0..6]);
-let word= first_word(&my_string[..]);
-let word= first_word(&my_string);
-
-let string_literal= "hello_world";
-
-let word= first_word(&string_literal[..]);
-let word= first_word(&string_literal[0..8]);
-let word= first_word(string_literal);
-
-
-
-
-let a= [1,3,3,4,5,5];
-let slice= &a[1..6];
-
-assert_eq!(slice , &[2,3]);
-
-
-
-
+#[derive(Debug)]
+struct Rectangle{
+    width:u32,
+    height:u32,
 }
 
-fn first_word(s:&str) -> &str{
-    let bytes = s.as_bytes();
-    for(i , &item) in bytes.iter().enumerate(){
-        if(item == b' '){
-            return &s[0..i];
-        }
+impl Rectangle{
+    fn area(&self) -> u32{
+self.width * self.height
     }
-    &s[..]
+
+    
+
+
+    fn can_hold(&self , other:&Rectangle) -> bool{
+           if self.width > other.width   && self.height > other.height
+           {
+            true
+           }
+           else{
+            false 
+           }
+    }
+
+
+    fn square(size:u32) -> Self{
+        Self { width: (size), height: (size) }
+    }
 }
+
+impl Rectangle{
+    fn width(&self) -> bool{
+        self.width >0
+    }
+}
+fn main(){
+    let scale=4;
+    let rect1= Rectangle{
+        width:dbg!(30*scale),
+        height:50
+
+    };
+    // if rect1.width(){
+    //     println!("the rectangle has a nonzero width {}", rect1.width);
+    // }
+    // println!("area = {} sqaure metres", rect1.area());
+
+    // // println!("rect1= {:#?}", rect1)d
+    // dbg!(&rect1);
+
+    let rect2= Rectangle{
+        width:dbg!(10*scale),
+        height:40
+    };
+
+        println!("Rect1 can hold rect2 -> {}", rect1.can_hold(&rect2));
+        println!("Rec2 can hold rect1 -> {}", rect2.can_hold(&rect1));
+
+
+        let sq= Rectangle::square(5);
+
+}
+
+
