260bf2a57de5c395a16baf17830cca79f4c8c5a4
Divyansh Gupta <divyanshguptaekana123@gmail.com>
2023-12-29 01:26:58 +0530

    chore(restaruant): organise the code

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,84 +1,8 @@
-use std::fmt::Result as rf;
-use std::io::Result as ri;
-use::rand::Rng;
+mod front_to_house;
 
-fn f1(){
-    let secret_number= rand::thread_rng().gen_range(1..=100);
-    let n= HashMap::new();
-}
-fn function1() -> rf{
-
-}
-
-fn function2() -> ri<()> {
-
-}
+ pub use crate::front_to_house::hosting;
 
 
-
-mod front_of_house{
-    pub mod hosting{
-        pub fn add_to_waitlist(){}
-        
-    }
-}
-
-
-mod back_of_house{
-
-
-    // pub enum Appetizer{
-    //     soup,
-    //     Salad,
-    // }
-    
-    // pub struct Breakfast
-    // {
-    //     pub  toast: String,
-    //     seasonal_fruit:String,
-    // }
-    
-    // impl Breakfast{
-    //     // #[derive(Debug)]
-    //    pub fn summer(toast: &str) -> Breakfast{
-            
-    //         Breakfast{
-    //             toast: String::from(toast),
-                // seasonal_fruit:String::from("papita"),
-    //         }
-    //     }
-    // }
-
-
-    // fn fix_incorrect_order(){
-    //     cook_order();
-    //     super::deliver_order();
-    // }
-
-    // fn cook_order(){}
-}
-
-
-mod customer {
-       use crate::front_of_house::hosting;
-     pub fn eat_at_restaurant(){
-        hosting::add_to_waitlist();
-    }
-   }
-
-    fn deliver_order(){}
-
-
-//     mod serving{
-//         fn take_order(){}
-
-//         fn serve_order(){}
-
-//         fn take_payments(){}
-//     }
-// }
-
-
-// restaurant::front_of_house::hosting::add_to_waitlist();
-
-// restaurant::hosting::add_to_waitlist();
+ pub fn eat_at_restaurant(){
+    hosting::add_to_waitlist;
+ }
--- restaurant/target/debug/deps/restaurant-fbb3ecbf444b34d4.d
+++ restaurant/target/debug/deps/restaurant-fbb3ecbf444b34d4.d
@@ -1,7 +1,9 @@
-/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/librestaurant-fbb3ecbf444b34d4.rmeta: src/lib.rs
+/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/librestaurant-fbb3ecbf444b34d4.rmeta: src/lib.rs src/front_to_house.rs src/front_to_house/hosting.rs
 
-/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/librestaurant-fbb3ecbf444b34d4.rlib: src/lib.rs
+/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/librestaurant-fbb3ecbf444b34d4.rlib: src/lib.rs src/front_to_house.rs src/front_to_house/hosting.rs
 
-/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/restaurant-fbb3ecbf444b34d4.d: src/lib.rs
+/home/divyanshgupta/rust_tutorial/restaurant/target/debug/deps/restaurant-fbb3ecbf444b34d4.d: src/lib.rs src/front_to_house.rs src/front_to_house/hosting.rs
 
 src/lib.rs:
+src/front_to_house.rs:
+src/front_to_house/hosting.rs:
