39de3a3de0200cd0c0d579a4a1203edcb900758c
Divyansh Gupta <divyanshguptaekana123@gmail.com>
2023-12-30 21:31:12 +0530

    feat(learn generics): learn generics

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,100 +1,88 @@
-use std::fs:: File;
-// use std::io::ErrorKind;
-use std::fs;
-use std::io;
+use std::cmp::PartialOrd;
 
+struct Point<X1, Y1> {
+    x:X1,
+    y:Y1,
+}
 
-
-// use std::io::{self,Read};
-fn main(){
-    // panic!("crash and burn");
-
-
-    // fn another();
-
-    // let greeting_file_result= File::open("./hello.txt");
-    
-//     let greeting_file = match greeting_file_result{
-//        Result::Ok(file) => file,
-//         Err(error) => match error.kind(){
-//                    ErrorKind::NotFound => match File::create("hello.txt"){
-//                     Ok(f) => f,
-//                     Err(e)=> panic!("problem creating the file ={}", e),
-//                    },
-//                    other_error => {
-//                     panic!("Problem opening the file={}", other_error);
-//                    }
-//         }
-// };
-
-//  let greeting_file= File::open("hello.txt").unwrap_or_else(|error|{
-//     if error.kind() == ErrorKind::NotFound {
-//         File::create("hello.txt").unwrap_or_else(|error|{
-//             panic!("Problem creating the file: {:?}",error);
-//         })
-//     }
-//     else{
-//         panic!("Problem openign the file");
-//     }
-//  });
-
-
-// let greeting_file= File::open("hello.txt").unwrap();
-
-
-
-// let greeting_file= File::open("hello.txt")
-                                            //  .expect("hello.txt should be included in this project");
-
-
-    let greeting_file= File::open("hello.txt")?;                                        
-
-
+impl<X1,Y1> Point<X1,Y1>{
+    fn mixup<X2,Y2>(&self, other:Point<X2,Y2>)-> Point<X1,Y2>{
+       Point { x:self.x , y:other.y }
+    }
 }
 
 
-// fn read_username_from_file() -> Result< String, io::Error>{  // Result<t,e>
-
-//     let username_file_result= File::open("hello.txt");
-
-//     let mut username_file= match username_file_result{
-//      Ok(file) =>file,
-//       Err(e) => return Err(e),
-//     };
-
-//     let mut username= String::new();
-
-//     match username_file.read_to_string(&mut username){
-//         Ok(_)=> Ok(username),
-//         Err(e) => Err(e),
+// impl<U> Point<f32> {
+//     fn x(&self) -> &U{
+//         &self.x
 //     }
+// }
 
-// } 
+// impl Point<f64> {
+//     fn distnace_form_origin(&self) -> f64{
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
 
+ 
+// enum Option<T>{
+//     Some(T),
+//     None
+// }
 
-// fn read_username_from_file() -> Result< String, io::Error>{  // Result<t,e>
+// enum Result<T,E> {
+//     Ok(T),
+//     Err(E),
+// }
 
-//     let mut  username_file= File::open("hello.txt")?;
-
-//     // let mut username_file= match username_file_result{
-//     //  Ok(file) =>file,
-//     //   Err(e) => return Err(e),
-//     // };
-
-//     let mut username= String::new();
-
-//     username_file.read_to_string(&mut username)?;
-//     // match username_file.read_to_string(&mut username){
-//     //     Ok(_)=> Ok(username),
-//     //     Err(e) => Err(e),
-//     // }
-//     Ok(username)
-    
-
-// } 
-
+enum Option{
+    Some(f64),
+    None
+}
 
 
+fn main(){
+
+
+//     let number_list= vec![2,3,45,6];
+//    let result= largest(&number_list);
+
+//    let arr= [1,2,3,4];
+// let result= largest(&arr);
+
+//     println!("the largest no is = {}", result);
+
+//     let number_list = vec!['e','r','t','u'];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+// let integer= Point{x:5, y:7};
+// let float= Point{x:"edffdsfd",y:"xcdvn" };
+
+// let mix= Point{x:6.0, y:7.0};
+// let result= mix.distnace_form_origin();
+
+let p1= Point{x:5, y:12.7};
+let p2= Point{x:"Hello", y:'c'};
+
+let p3= p1.mixup(p2);
+ println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 
 
 
+
+ let integer= Some(5);
+ let float= Some(6.0);
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T{
+    let mut  large= list.get(0).unwrap();
+
+    for number in list{
+        if large < number{
+            large = number;
+        }
+    };
+    large
+}
